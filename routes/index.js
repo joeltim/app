985e12734b4d767fd0d6dda3552bbd062483bc72
Joel <74513202+joeltim@users.noreply.github.com>
2021-03-07 18:52:15 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({message: 'alive'});
+  res.json(I haven\'t had a chance to fix this yet.);
 });
 
 module.exports = router;
