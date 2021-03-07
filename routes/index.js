338ae35691c917b9fae04be36f22ff9086d737d3
Joel <74513202+joeltim@users.noreply.github.com>
2021-03-07 18:53:49 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json(I haven\'t had a chance to fix this yet.);
+  res.json({I havent had a chance to fix this yet.});
 });
 
 module.exports = router;
